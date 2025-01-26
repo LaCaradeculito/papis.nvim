8d32d11f34b97dc7f6a029c52d2b7279561bf3dc
Marcelo Santorelli <mhsantorelli@gmail.com>
2025-01-26 02:08:21 -0300

    2do intento

---
+++ lua/plugins/papis.lua
@@ -0,0 +1,17 @@
+return {
+  "jghauser/papis.nvim",
+  dependencies = {
+    "kkharji/sqlite.lua",
+    "MunifTanjim/nui.nvim",
+    "pysan3/pathlib.nvim",
+    "nvim-neotest/nvim-nio",
+    -- if not already installed, you may also want:
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("papis").setup({
+    -- Your configuration goes here
+    })
+  end,
+} 
